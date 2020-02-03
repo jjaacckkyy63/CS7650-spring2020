b8faaf81cb178eaa7911a1c19b9d4d0397c5fc06
劉翼傑 <jjaacckkyy63@gmail.com>
2020-02-03 02:14:02 -0500

    test

--- hw1/h2_text_classification/classifiers.py
+++ hw1/h2_text_classification/classifiers.py
@@ -159,7 +159,7 @@ class LogisticRegressionClassifier(HateSpeechClassifier):
 
                 self.weight = self.gradient_descent(X_mini, Y_mini, self.weight, self.lr)
                 y_preds = []
-                for i in len(X_mini):
+                for i in range(len(X_mini)):
                     y_preds.append(self.sigmoid(np.dot(X_mini[i], betas.T)))
                     
                 y_pred = np.dot(X_mini, self.weight.T)
